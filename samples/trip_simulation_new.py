41903056fcaad9e5737b6f9c161c0fc892f935dd
Felix Zenk <33557089+felix-zenk@users.noreply.github.com>
2020-12-13 00:04:25 +0100

    Update trip_simulation_new.py

--- samples/trip_simulation_new.py
+++ samples/trip_simulation_new.py
@@ -1,5 +1,5 @@
 #
-# REQURIES ICEPORTAL-APIS v1.0.7 or newer
+# REQUIRES ICEPORTAL_APIS v1.0.7 or newer
 #
 
 # Import
@@ -7,32 +7,25 @@ import iceportal_apis as ipa
 from datetime import datetime, timedelta
 from time import sleep
 
-from sample_data_supplier import get_sample_data_status, get_sample_data_trip, refresh_data
-ipa.get_status = lambda: get_sample_data_status()
-ipa.get_trip = lambda: get_sample_data_trip()
-
 def clear():
     from os import system, name
     system('cls' if name=='nt' else 'clear')
 
+def end():
+    print('\n\nBeende...')
+    sleep(3)
+    exit()
 ############################    
 # RUN
 ############################
-finished = False
+try:
+    ipa.get_station_names()
+except ipa.NotOnTrainException:
+    print('Du musst dich in einem Zug befinden um diese Funktion nutzen zu können!')
+    end()
 ziel = None
+finished = False
 bahnhoefe = ipa.get_station_names()
-
-def chooseMultiplier():
-    try:
-        mul = float(input('(ENTER für Echtzeit)\nMultiplikator für Zeit: '))
-    except:
-        mul = 1.0
-    if mul < 1.0:
-        clear()
-        print('Es sind nur Zahlen >= 1.0 erlaubt')
-        return chooseMultiplier()
-    else:
-        return mul
 
 def chooseDestination():
     def choose():
@@ -78,19 +71,9 @@ def chooseDestination():
                 return chooseFiltered(possible_stations)
     return choose()
 
-#############################
-try:
-    ziel = chooseDestination()
-    print(f'\n - "{ziel}" gewählt\n')
-    multiplier = chooseMultiplier()
-    refresh_data()
-except:
-    ziel = None
+ziel = chooseDestination()
 if ziel in bahnhoefe:
     try:
-        simulated_time = datetime.now()
-        if multiplier > 60:
-            simulated_time = simulated_time.replace(second=0)
         while not finished:
             try:
                 status, trip = ipa.get_all()
@@ -98,17 +81,20 @@ if ziel in bahnhoefe:
                 print('Verbindung verloren.')
                 break
             clear()
-            print(f'Aktuelle Zeit: {simulated_time.strftime("%d.%m.%Y %H:%M:%S")}')
+            print(f'Aktuelle Zeit: {datetime.now().strftime("%d.%m.%Y %H:%M:%S")}')
             print(f'Dies ist: {ipa.get_train_type(status_call=status)} {ipa.get_trip_id(trip_call=trip)} nach {ipa.get_final_station_name(trip_call=trip)}')
             print()
             try:
-                if ipa.convert_time_to_string(ipa.get_time_until_next_arrival(trip_call=trip), locale="de", no_seconds=True) != '':
-                    print(f'Nächste Ankunft: {ipa.get_arrival_time(evaNr=ipa.get_next_station_eva_number(trip_call=trip)).strftime("%H:%M")} ({ipa.convert_time_to_string(ipa.get_time_until_next_arrival(trip_call=trip), locale="de", no_seconds=True)})')
+                if ipa.convert_time_to_string(ipa.get_time_until_next_arrival(trip_call=trip), locale="de") != '':
+                    print(f'Nächste Ankunft: {ipa.get_arrival_time(evaNr=ipa.get_next_station_eva_number(trip_call=trip), trip_call=trip).strftime("%H:%M")} ({ipa.convert_time_to_string(ipa.get_time_until_next_arrival(trip_call=trip), locale="de", no_seconds=True)})')
                 else:
-                    print(f'Nächste Ankunft: {ipa.get_arrival_time(evaNr=ipa.get_next_station_eva_number(trip_call=trip)).strftime("%H:%M")} (Jetzt)')
+                    print(f'Nächste Ankunft: {ipa.get_arrival_time(evaNr=ipa.get_next_station_eva_number(trip_call=trip), trip_call=trip).strftime("%H:%M")} (Jetzt)')
             except:
                 pass
             print(f'Nächste Station: {ipa.get_next_station_name(trip_call=trip)} (Gleis {ipa.get_next_track(trip_call=trip)})')
+            
+            print(f'Noch {"{:.1f}".format(int(ipa.get_station_distance(evaNr=ipa.get_next_station_eva_number(trip_call=trip), trip_call=trip))/1000)}km bis zum Bahnhof')
+            
             if not ipa.get_next_station_name(trip_call=trip) == ziel:
                 try:
                     if ipa.convert_time_to_string(ipa.get_time_until_next_departure(trip_call=trip), locale="de", no_seconds=True) != '':
@@ -125,23 +111,16 @@ if ziel in bahnhoefe:
             else:
                 print(f'\nDer Zug ist pünktlich')
             print(f'\nAktuelle Geschwindigkeit: {ipa.get_speed(status_call=status)}km/h')
-            if simulated_time > ipa.get_arrival_time(evaNr=ipa.get_final_station_eva_number(trip_call=trip), trip_call=trip):
+            if datetime.now() > ipa.get_arrival_time(evaNr=ipa.get_final_station_eva_number(trip_call=trip), trip_call=trip):
                 finished = True
                 print('Der Zug ist am Ziel angekommen')
             else:
                 try:
                     print(f'Ausstieg in {ziel}: {ipa.get_arrival_time(station_name=ziel, trip_call=trip).strftime("%H:%M")} ({ipa.convert_time_to_string(ipa.get_time_until_arrival(station_name=ziel, trip_call=trip), locale="de", no_seconds=True)})')
-                    print(f'Noch {int(ipa.get_station_distance(evaNr=ipa.get_next_station_eva_number(trip_call=trip), status_call=status, trip_call=trip))}m bis zum nächsten Bahnhof')
                 except ipa.NotInFutureException:
                     pass
-            if multiplier <= 60:
-                sleep(1/multiplier)
-                simulated_time+=timedelta(seconds=1)
-            else:
-                sleep(60/multiplier)
-                simulated_time+=timedelta(minutes=1)
+                    
+            sleep(0.5)
     except KeyboardInterrupt:
         pass
-print('\n\nBeende...')
-sleep(2)
-ipa.autoupdate()
+end()
