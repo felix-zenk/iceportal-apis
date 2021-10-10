97c51b3a398664cdd5741070a2241f555cdeb016
Felix <felix.zenk@web.de>
2021-10-10 19:33:15 +0200

    Added a backup synchronous interface

--- iceportal_apis/interfaces.py
+++ iceportal_apis/interfaces.py
@@ -1,11 +1,11 @@
 import asyncio
-from datetime import datetime
-from json import JSONDecodeError
-from threading import Thread
-from time import sleep
-from typing import Tuple, Union, Type, List
-
-from aiohttp import ClientSession, ContentTypeError, ClientError
+import datetime
+import json
+import threading
+import time
+import typing
+import aiohttp
+import requests
 
 from .constants import (URL_STATUS, URL_TRIP, URL_CONNECTIONS, URL_POIS)
 from .exceptions import (NetworkException, ApiException, NotOnTrainException)
@@ -20,10 +20,10 @@ class ApiInterface:
         self.trip: dict = {}
         self.connections: dict = {}
         self.stations: dict = {}
-        self.pois: List = []
+        self.pois: typing.List = []
         self.eva_nr_2_name: dict = {}
         self.name_2_eva_nr: dict = {}
-        self._auto_refresh_thread: Thread = Thread(target=None, daemon=True)
+        self._auto_refresh_thread: threading.Thread = threading.Thread(target=None, daemon=True)
         self._auto_refresh_switch: bool = False
         self._AUTO_REFRESH_INTERVAL: int = 1
         self._event_loop = asyncio.get_event_loop()
@@ -33,13 +33,13 @@ class ApiInterface:
         """Requests data from 'url' and parses it as a dictionary
         """
         self.interface_status = InterfaceStatus.FETCHING
-        async with ClientSession(loop=self._event_loop) as session:
+        async with aiohttp.ClientSession(loop=self._event_loop) as session:
             async with session.get(url) as response:
                 try:
                     return await response.json()
-                except (JSONDecodeError, ContentTypeError) as e:
+                except (json.JSONDecodeError, aiohttp.ContentTypeError) as e:
                     raise NotOnTrainException() from e
-                except ClientError as e:
+                except aiohttp.ClientError as e:
                     raise NetworkException from e
 
     async def _get_status(self) -> None:
@@ -62,7 +62,7 @@ class ApiInterface:
         """
         self.connections[eva_nr] = await self._request_json(URL_CONNECTIONS.format(eva_nr))
 
-    async def _get_pois(self, start_pos: Tuple[str, str], end_pos: Tuple[str, str]):
+    async def _get_pois(self, start_pos: typing.Tuple[float, float], end_pos: typing.Tuple[float, float]):
         """
         Refreshes points of interest in the rectangular area spanned by start_pos and end_pos
         :param start_pos: The first corner position
@@ -102,14 +102,14 @@ class ApiInterface:
 
     def _auto_refresh(self) -> None:
         while self._auto_refresh_switch:
-            start = datetime.now()
+            start = datetime.datetime.now()
             try:
                 self.refresh()
             except ApiException:
                 self._auto_refresh_switch = False
                 raise
-            delta = datetime.now() - start
-            sleep(0 if delta.total_seconds() > self._AUTO_REFRESH_INTERVAL
+            delta = datetime.datetime.now() - start
+            time.sleep(0 if delta.total_seconds() > self._AUTO_REFRESH_INTERVAL
                   else self._AUTO_REFRESH_INTERVAL - delta.seconds - delta.microseconds / 1000000)
 
     def set_auto_refresh(self, auto_refresh: bool, interval: int = None):
@@ -117,9 +117,9 @@ class ApiInterface:
             self._AUTO_REFRESH_INTERVAL = interval
         if auto_refresh:
             self._auto_refresh_switch: bool = True
-            self._auto_refresh_thread = Thread(target=self._auto_refresh,
-                                               name=self.__class__.__name__+"ConnectionThread",
-                                               daemon=True)
+            self._auto_refresh_thread = threading.Thread(target=self._auto_refresh,
+                                                         name=self.__class__.__name__+"ConnectionThread",
+                                                         daemon=True)
             self._auto_refresh_thread.start()
         else:
             self._auto_refresh_switch = False
@@ -128,8 +128,49 @@ class ApiInterface:
             #    self._auto_refresh_thread.join()
 
 
+class SynchronousApiInterface(ApiInterface):
+    def __init__(self):
+        super(SynchronousApiInterface, self).__init__()
+
+    def _request_json(self, url: str) -> dict:
+        try:
+            response = requests.get(url)
+            try:
+                return response.json()
+            except json.JSONDecodeError:
+                raise NotOnTrainException()
+        except requests.exceptions.RequestException:
+            raise NetworkException()
+
+    def _get_status(self) -> None:
+        self.status = self._request_json(URL_STATUS)
+
+    def _get_trip(self):
+        self.trip = self._request_json(URL_TRIP)
+
+    def _get_connections(self, eva_nr: str):
+        self.connections[eva_nr] = self._request_json(URL_CONNECTIONS.format(eva_nr))
+
+    def _get_pois(self, start_pos: typing.Tuple[float, float], end_pos: typing.Tuple[float, float]):
+        self.pois.append({
+            'start_pos': start_pos,
+            'end_pos': end_pos,
+            'data': self._request_json(URL_POIS.format(start_pos[0], start_pos[1], end_pos[0], end_pos[1]))
+        })
+
+    def refresh(self) -> None:
+        self._get_status()
+        self._get_trip()
+        for eva_nr, name in [(stop["station"]["evaNr"], stop["station"]["name"])
+                             for stop in self.trip["trip"]["stops"]]:
+            self.name_2_eva_nr[name] = eva_nr
+            self.eva_nr_2_name[eva_nr] = name
+            self._get_connections(eva_nr)
+
+
 class TestInterface(ApiInterface):
-    def __init__(self, simulation_type: Type[Union[StaticSimulation, DynamicSimulation]] = StaticSimulation):
+    def __init__(self,
+                 simulation_type: typing.Type[typing.Union[StaticSimulation, DynamicSimulation]] = StaticSimulation):
         self.simulation = simulation_type()
         self.simulation.start()
         super(TestInterface, self).__init__()
